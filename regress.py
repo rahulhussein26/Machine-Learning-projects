c4dee56d04c95eb527874a06bc9761ff6c1717aa
Rahul <rahul.h26@outlook.com>
2025-08-28 00:03:28 +0100

    created the function for the equation

--- regress.py
+++ regress.py
@@ -1,4 +1,42 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
+from scipy import stats 
 
-plt.plot((1,2,3))
+x = np.arange(0, 4*np.pi, step=0.01)
+y = np.sin(x)
+
+plt.figure(figsize=(10,8))
+plt.plot(x,y)
+plt.grid(True)
+
+
+x_val = np.arange(0, 45, step=5)
+print(x_val)
+
+print(x_val)
+
+y_val = [i for i in x_val if i == 2*i]
+y_list = [] 
+
+for i in x_val: 
+    y = 2*i 
+    y_list.append(y)
+
+print("checking if the y values are properly working")
+
+print(y_list)
+y_list_new = [] 
+
+
+for i in y_list: 
+    whl_num = int(i)
+    y_list_new.append(whl_num)
+    
+    
+slope, intercept, r, p, std_err = stats.linregress(x_val, y_list_new)
+
+def func(x):
+    return slope*x_val + intercept
+
+
+# need to create the model 
